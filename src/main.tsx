2c8c06f7db4cbf82f30f60e61af20991dd732117
Aponi James <115168597+1436799528@users.noreply.github.com>
2026-09-15 22:46:03 +0100

    Register EduReach PWA runtime

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import HubApp from './HubApp';
+import PwaRegister from './components/PwaRegister';
 import './app.css';
 import './hub.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <PwaRegister />
     <HubApp />
   </StrictMode>,
 );
