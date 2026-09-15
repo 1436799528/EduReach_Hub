a3e5722807c9f4335c197e90d7492a0834aebc68
Aponi James <115168597+1436799528@users.noreply.github.com>
2026-09-15 20:22:20 +0100

    ui: load action-first card styling

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './app.css';
 import './home-refresh.css';
+import './card-polish.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
