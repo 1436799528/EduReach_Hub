2ff651d8268aaa7ee6b5b21a04a87cfef308e467
Aponi James <115168597+1436799528@users.noreply.github.com>
2026-09-15 21:22:38 +0100

    Use database-backed service tracking route

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,27 @@
-import { StrictMode } from 'react';
+import { StrictMode, useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import IntegrationBridge from './IntegrationBridge.tsx';
+import ServiceTrackOverride from './ServiceTrackOverride.tsx';
 import './app.css';
 import './home-refresh.css';
 import './card-polish.css';
 
+function RootRouter() {
+  const [path, setPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handlePopState = () => setPath(window.location.pathname);
+    window.addEventListener('popstate', handlePopState);
+    return () => window.removeEventListener('popstate', handlePopState);
+  }, []);
+
+  return path.replace(/\/$/, '') === '/services/track' ? <ServiceTrackOverride /> : <App />;
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <IntegrationBridge />
-    <App />
+    <RootRouter />
   </StrictMode>,
 );
