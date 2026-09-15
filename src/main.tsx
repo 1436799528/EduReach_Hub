f88cb719bfd486211ac0fb97fdb18f3875cfb727
Aponi James <115168597+1436799528@users.noreply.github.com>
2026-09-15 22:38:42 +0100

    feat: switch app entry to EduReach Hub architecture

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,11 @@
-import { StrictMode, useEffect, useState } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import HighDensityHome from './HighDensityHome';
-import IntegrationBridge from './IntegrationBridge.tsx';
-import ServiceTrackOverride from './ServiceTrackOverride.tsx';
-import { initUiPolish } from './uiPolish';
+import HubApp from './HubApp';
 import './app.css';
-import './home-refresh.css';
-import './card-polish.css';
-import './service-track.css';
-import './dense-portal.css';
-
-function RootRouter() {
-  const [path, setPath] = useState(window.location.pathname);
-
-  useEffect(() => {
-    const handlePopState = () => setPath(window.location.pathname);
-    window.addEventListener('popstate', handlePopState);
-    return () => window.removeEventListener('popstate', handlePopState);
-  }, []);
-
-  const cleanPath = path.replace(/\/$/, '') || '/';
-  if (cleanPath === '/') return <HighDensityHome />;
-  return cleanPath === '/services/track' ? <ServiceTrackOverride /> : <App />;
-}
+import './hub.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <IntegrationBridge />
-    <RootRouter />
+    <HubApp />
   </StrictMode>,
 );
-
-initUiPolish();
